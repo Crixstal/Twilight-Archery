bf47c02ede981258da0d230bb20b7bdcfea84458
Manon Méhalin <m.mehalin@student.isartdigital.com>
2021-11-19 13:22:23 +0100

    [master] init commit

---
+++ Source/TwilightArchery/TwilightArcheryGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TwilightArcheryGameMode.h"
+#include "TwilightArcheryCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ATwilightArcheryGameMode::ATwilightArcheryGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
---
+++ Source/TwilightArchery/TwilightArcheryGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TwilightArcheryGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATwilightArcheryGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATwilightArcheryGameMode();
+};
+
+
+
