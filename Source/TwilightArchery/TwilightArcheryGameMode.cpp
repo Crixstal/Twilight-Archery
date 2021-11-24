921981da4e448b7eed6a4f582c4a30d3f801b256
A Gerbeaud <a.gerbeaud@student.isartdigital.com>
2021-11-24 11:17:37 +0100

    [Splitscreen] Fixed P2 not shooting more than one arrow, animations and UI not showing

--- Source/TwilightArchery/TwilightArcheryGameMode.cpp
+++ Source/TwilightArchery/TwilightArcheryGameMode.cpp
@@ -10,7 +10,7 @@
 ATwilightArcheryGameMode::ATwilightArcheryGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Player/TwilightArcheryCharacter_BP"));
 	
 	if (PlayerPawnBPClass.Class != NULL)
 		DefaultPawnClass = PlayerPawnBPClass.Class;
